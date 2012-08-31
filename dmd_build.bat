3e4b256b0cf8a9c903ffe0f4b14db464929c0630
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2012-08-31 04:11:52 +0200

    fix build script.

--- dmd_build.bat
+++ dmd_build.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal EnableDelayedExpansion
 set "files="
-for %%i in (win32\*.d;win32\*.di) do set files=!files! %%i
+for %%i in (win32\*.d) do set files=!files! %%i
 dmd -I. -version=Unicode -version=WindowsXP -lib -ofdmd_win32_x32.lib %files%
